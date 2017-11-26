4530e05284202568874f3416f628c54d09efb5be
Microsmsm <drusama2@gmail.com>
2017-11-26 21:04:45 +0200

    refine code and error handling

--- require-params.js
+++ require-params.js
@@ -1,63 +1,67 @@
-//module.exports = requireParams
-
-/**
- * Utility package to faster require certain paramerters and using them during the post request handling
- * 
- * @param  {{Response:Response,Request:Request}} mainObject - contains Request and Response
- * @param  {Array} requiredParamsArray - array of required params during code excution
- * @returns {Boolean|Array} - returns boolean to stop excution if defaultResponse is true sending response of default message to the user "or" sending array of missed required parameters array to handle them within the code
- */
-function missingPrams({
-    req,
-    res
-}, requiredParamsArray) {
-    //handling errors
-    if (!{
-            req,
-            res
-        }) return new Error('send {req,res} for the first argument')
-    if (!(requiredParamsArray instanceof Array)) return new Error('you must be using array for the second argument')
-    if (requiredParamsArray < 1) return new Error('not found Required parameters for the second argument please use array of string parameters')
-
-    //Assigning body keys to array optimizing performance
-    const bodyObjKeys = Object.keys(req.body);
-    //extracting the missing paramerters
-    const missedParams = requiredParamsArray.filter(param => !bodyObjKeys.includes(param))
-
-    //or use custom one?? here's the array of missedParams
-    return missedParams;
-}
-
-
-//requiredParamsArray
-//path
-exports.express = function (path, requiredParams,forceParams) {
+module.exports = function (path, requiredParamsArray, forceRequireParams) {
+    //handle errors
+    if (!(requiredParamsArray instanceof Array)) {
+        return new Error('you must be using array for the second argument')
+    }
+    if (requiredParamsArray < 1) {
+        return new Error('not found Required parameters for the second argument please use array of string' +
+                ' parameters')
+    }
+    
     return function (req, res, next) {
+        const reqParams = req.body
+        console.log(reqParams);
+        console.log(req.path, path);
+        //specific path for the middleware
+        if (req.path === path) {
 
-        //Only run when req.path equles path
-        if(req.path === path){
+            //figure the missing required parameters
+            const missingRequiredParams = getMissingPrams(reqParams, requiredParamsArray);
+            console.log(missingRequiredParams);
 
-            const missingRequiredParams = missingPrams({
-                req,
-                res
-            }, requiredParams);
+            //is there missing parameters?
+            if (missingRequiredParams.length > 0) {
 
-            if(missingRequiredParams.length > 0){
-                let msg = missingRequiredParams.length > 1 ?
-                " parameters > NOT FOUND" :
-                " parameter > NOT FOUND"
+                //more than one parameter missing? ...propper display message
+                let msg = missingRequiredParams.length > 1
+                    ? " parameters > NOT FOUND"
+                    : " parameter > NOT FOUND"
                 let error = missingRequiredParams.join() + msg
-
-                if(forceParams){
-                    res.status(400).send(error);
-                }else{
+                if (forceRequireParams) {
+                    res
+                        .status(400)
+                        .send(error);
+                } else {
                     req.missingParams = missingRequiredParams;
                     next();
                 }
+            } else {
+                next();
             }
-
-        }else{
-            next()            
         }
     }
+}
+/**
+ * function to detect missing paramerters
+ *
+ * @param  {Request:Request} reqParams - contains Request and Response
+ * @param  {String[]} requiredParamsArray - array of required params during code excution
+ * @returns {String[]} - sending array of missed required parameters array to handle them within the code
+ */
+function getMissingPrams(reqParams, requiredParamsArray) {
+    //handling errors
+    if (!reqParams) 
+        return new Error('send reqParamsect for the first argument')
+    if (!(requiredParamsArray instanceof Array)) 
+        return new Error('you must be using array for the second argument')
+    if (requiredParamsArray < 1) 
+        return new Error('not found Required parameters for the second argument please use array of string' +
+                ' parameters')
+
+        //Assigning body keys to array optimizing performance
+    const reqParamsArray = Object.keys(reqParams);
+    //extracting the missing paramerters
+    const missedParams = requiredParamsArray.filter(param => !reqParamsArray.includes(param))
+    //here's the array of missedParams
+    return missedParams;
 }
